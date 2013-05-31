91ce9ff336000914d23c551b1b3c83efec4e8e9e
tariqul-islam <ponir.bd@hotmail.com>
2013-05-31 21:27:21 +0600

    Update ulam_spiral.m

--- ulam_spiral.m
+++ ulam_spiral.m
@@ -1,3 +1,18 @@
+%{
+ULAM_SPIRAL
+Creates (x,y) co-ordinates of ULAM SPIRAL in the first quadrant
+
+[x,y] = ulam_spiral(N), outputs (x,y) co-ordinates
+x = ulam_spiral(N), outputs a two row vercor, first row gives the x cordinates, second row gives y co-ordinates
+
+Written by,
+Tariqul Islam Ponir
+ponir.bd @ hotmail.com
+%}
+
+
+
+
 function [xpos,ypos] = ulam_spiral(N)
     if length(N) ~= 1
         error('Invalid Argument');
