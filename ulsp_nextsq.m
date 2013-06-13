bbb50e34c3f7d33b7a990c82fa6b963b956381f3
Tariqul Islam Ponir <ponir.bd@hotmail.com>
2013-06-13 23:28:27 +0600

    hmm

--- ulsp_nextsq.m
+++
@@ -1,6 +0,0 @@
-function y = ulsp_nextsq(x)
-    y=ceil(sqrt(x));
-    if mod(y,2)==0
-        y=y+1;
-    end
-end
--- ulsp_prevsq.m
+++
@@ -1,6 +0,0 @@
-function y = ulsp_prevsq(x)
-    y=floor(sqrt(x));
-    if mod(y,2)==0
-        y=y-1;
-    end
-end
